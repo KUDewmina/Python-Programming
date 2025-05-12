0b439e54dc1f9de60b4ee096ae6ecbd51d6ad38e
K.Udul Dewmina <165687248+KUDewmina@users.noreply.github.com>
2025-05-13 01:03:24 +0530

    Update Boolean Combinations.py

--- Combinations/Boolean Combinations.py
+++ Combinations/Boolean Combinations.py
@@ -3,3 +3,7 @@ def generate_combinations(n):  # This function generates all possible binary com
     print([format(i, f'0{n}b') for i in range(2 ** n)])
 
 generate_combinations(3)
+
+ # format(i, f'0{n}b') -> This converts number i into a binary string.
+
+# Output -> ['000', '001', '010', '011', '100', '101', '110', '111']
